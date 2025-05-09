4402cd6d79acecf619627024ed153d35fc668bef
pedrocatelani <pedrocatelani@gmail.com>
2025-05-08 22:58:55 -0300

    feat(pages&components): Added Edit User and Deck Pages

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -5,122 +5,128 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 140,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(132, 213, 212, 212),
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: const Color.fromARGB(21, 0, 0, 0),
-            spreadRadius: 2,
-            blurRadius: 1,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      margin: EdgeInsets.all(8),
-      padding: EdgeInsets.all(8),
-      child: Column(
-        spacing: 8,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Deck Name etc...',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: () => Navigator.pushReplacementNamed(context, '/deck'),
+      child: Container(
+        height: 148,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(225, 255, 255, 255),
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(21, 0, 0, 0),
+              spreadRadius: 2,
+              blurRadius: 1,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        margin: EdgeInsets.all(8),
+        padding: EdgeInsets.all(8),
+        child: Column(
+          spacing: 8,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Deck Name etc...',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Spacer(),
-              Row(
-                spacing: 16,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  CircleAvatar(radius: 14, backgroundColor: Colors.pink),
-                  CircleAvatar(radius: 14, backgroundColor: Colors.pink),
-                  CircleAvatar(radius: 14, backgroundColor: Colors.pink),
-                ],
-              ),
-              SizedBox(width: 24),
-            ],
-          ),
-          Row(
-            children: [
-              SizedBox(width: 16),
-              Row(
-                spacing: 4,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    '97',
-                    style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-                  ),
-                  Text('SaS', style: TextStyle(fontSize: 16)),
-                ],
-              ),
-              Spacer(),
-              Column(
-                spacing: 4,
-                children: [
-                  Row(
-                    spacing: 6,
-                    children: [
-                      Icon(
-                        Icons.diamond_rounded,
-                        color: Color.fromARGB(255, 245, 220, 2),
+                Spacer(),
+                Row(
+                  spacing: 16,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CircleAvatar(radius: 14, backgroundColor: Colors.pink),
+                    CircleAvatar(radius: 14, backgroundColor: Colors.pink),
+                    CircleAvatar(radius: 14, backgroundColor: Colors.pink),
+                  ],
+                ),
+                SizedBox(width: 24),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(width: 16),
+                Row(
+                  spacing: 4,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      '97',
+                      style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text('11'),
-                      SizedBox(width: 16),
-                      Icon(
-                        Icons.diamond_outlined,
-                        color: Color.fromARGB(255, 255, 76, 17),
-                      ),
-                      Text('14'),
-                    ],
-                  ),
-                  Row(
-                    spacing: 6,
-                    children: [
-                      Icon(
-                        Icons.people_alt,
-                        color: const Color.fromARGB(255, 36, 112, 255),
-                      ),
-                      Text('12'),
-                      SizedBox(width: 16),
-                      Icon(
-                        Icons.no_accounts,
-                        color: Color.fromARGB(255, 255, 76, 17),
-                      ),
-                      Text('15'),
-                    ],
-                  ),
-                  Row(
-                    spacing: 6,
-                    children: [
-                      Icon(
-                        Icons.shield_sharp,
-                        color: Color.fromARGB(255, 36, 112, 255),
-                      ),
-                      Text('13'),
-                      SizedBox(width: 16),
-                      Icon(
-                        Icons.polymer_sharp,
-                        color: Color.fromARGB(255, 255, 76, 17),
-                      ),
-                      Text('16'),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(width: 24),
-            ],
-          ),
-        ],
+                    ),
+                    Text('SaS', style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                Spacer(),
+                Column(
+                  spacing: 4,
+                  children: [
+                    Row(
+                      spacing: 6,
+                      children: [
+                        Icon(
+                          Icons.diamond_rounded,
+                          color: Color.fromARGB(255, 245, 220, 2),
+                        ),
+                        Text('11'),
+                        SizedBox(width: 16),
+                        Icon(
+                          Icons.diamond_outlined,
+                          color: Color.fromARGB(255, 255, 76, 17),
+                        ),
+                        Text('14'),
+                      ],
+                    ),
+                    Row(
+                      spacing: 6,
+                      children: [
+                        Icon(
+                          Icons.people_alt,
+                          color: const Color.fromARGB(255, 36, 112, 255),
+                        ),
+                        Text('12'),
+                        SizedBox(width: 16),
+                        Icon(
+                          Icons.no_accounts,
+                          color: Color.fromARGB(255, 255, 76, 17),
+                        ),
+                        Text('15'),
+                      ],
+                    ),
+                    Row(
+                      spacing: 6,
+                      children: [
+                        Icon(
+                          Icons.shield_sharp,
+                          color: Color.fromARGB(255, 36, 112, 255),
+                        ),
+                        Text('13'),
+                        SizedBox(width: 16),
+                        Icon(
+                          Icons.polymer_sharp,
+                          color: Color.fromARGB(255, 255, 76, 17),
+                        ),
+                        Text('16'),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(width: 24),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/pages/deck.dart
@@ -0,0 +1,226 @@
+import 'package:crystal_navigation_bar/crystal_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:forgebase/components/background.dart';
+import 'package:forgebase/components/card.dart';
+import 'package:forgebase/components/cardgradient.dart';
+import 'package:iconly/iconly.dart';
+
+enum _SelectedTab { user, home, camera }
+
+class DeckPage extends StatefulWidget {
+  const DeckPage({super.key});
+
+  @override
+  State<DeckPage> createState() => _DeckPageState();
+}
+
+class _DeckPageState extends State<DeckPage> {
+  @override
+  Widget build(BuildContext context) {
+    _SelectedTab _selectedTab = _SelectedTab.user;
+
+    void _onTapChange(int index) {
+      setState(() {
+        _selectedTab = _SelectedTab.values[index];
+      });
+
+      Navigator.pushReplacementNamed(
+        context,
+        '/${_SelectedTab.values[index].name}',
+      );
+    }
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          CustomBackground(),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: ListView(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(137, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color.fromARGB(21, 0, 0, 0),
+                        spreadRadius: 2,
+                        blurRadius: 1,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  height: 92,
+                  child: Column(
+                    children: [
+                      Text('Deck Name etc...', style: TextStyle(fontSize: 22)),
+                      Text(
+                        'Mass Mutation',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    CircleAvatar(radius: 18, backgroundColor: Colors.pink),
+                    CircleAvatar(radius: 18, backgroundColor: Colors.pink),
+                    CircleAvatar(radius: 18, backgroundColor: Colors.pink),
+                  ],
+                ),
+                Container(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 12,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          '62',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '30',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '-2',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Base AERC',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Synergy',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Anti Synergy',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          '90',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text('SaS'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          '99%',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text('Percentil'),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(height: 32),
+                CardGradient(
+                  color: const Color.fromARGB(255, 255, 251, 25),
+                  title: Text(
+                    'Aembar',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(height: 16),
+                CardGradient(
+                  color: const Color.fromARGB(255, 255, 20, 20),
+                  title: Text(
+                    'Creature',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(height: 16),
+                CardGradient(
+                  color: const Color.fromARGB(255, 25, 190, 255),
+                  title: Text(
+                    'Speed',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(height: 16),
+                CardGradient(
+                  color: const Color.fromARGB(255, 90, 255, 25),
+                  title: Text(
+                    'Extras',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(height: 16),
+                CardGradient(
+                  color: const Color.fromARGB(255, 255, 25, 167),
+                  title: Text(
+                    'Card Count',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      extendBody: true,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: CrystalNavigationBar(
+          onTap: _onTapChange,
+          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+          indicatorColor: Colors.purple,
+          backgroundColor: const Color.fromARGB(255, 240, 240, 240),
+          enableFloatingNavBar: true,
+          items: [
+            CrystalNavigationBarItem(
+              icon: IconlyBold.user_2,
+              unselectedIcon: IconlyLight.user,
+              selectedColor: Colors.purple,
+              unselectedColor: Colors.purple,
+            ),
+            CrystalNavigationBarItem(
+              icon: IconlyBold.home,
+              unselectedIcon: IconlyLight.home,
+              selectedColor: Colors.purple,
+              unselectedColor: Colors.purple,
+            ),
+            CrystalNavigationBarItem(
+              icon: IconlyBold.category,
+              unselectedIcon: IconlyLight.category,
+              selectedColor: Colors.purple,
+              unselectedColor: Colors.purple,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
