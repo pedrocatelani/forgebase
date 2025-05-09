76e0625b4756d413e1ed12703f6a63118303c792
Gustavo <henriqueminghetti@gmail.com>
2025-05-08 22:07:02 -0300

    feat(idscanner): Added popup

--- lib/pages/idscanner.dart
+++ lib/pages/idscanner.dart
@@ -1,7 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:crystal_navigation_bar/crystal_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:forgebase/pages/scannerresult.dart';
+// import 'package:forgebase/pages/scannerresult.dart';
 import 'package:iconly/iconly.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -32,12 +32,12 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
           Expanded(
             child: MobileScanner(
               controller: cameraController,
-              onDetect: (capture) {
+              onDetect: (capture) async {
                 final List<Barcode> barcodes = capture.barcodes;
                 for (final barcode in barcodes) {
                   setState(() {
@@ -45,25 +45,174 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
                     if (qrCode != null && qrCode!.length >= 17) {
                       idDeck = qrCode!.substring(qrCode!.length - 17);
                       idDeck = idDeck!.replaceAll('-', '');
+
+                      cameraController.stop();
+
+                      showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Seu Deck foi encontrado!"),
+                            content: Text(
+                              "Deck de id: $idDeck!, é um belo deck! gostaria de adicionar ele aos seus baralhos?",
+                            ),
+                            actions: [
+                              TextButton(
+                                child: Text("Cancelar"),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Deck Nâo foi adicionado!"),
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  );
+
+                                  cameraController.start();
+                                },
+                              ),
+
+                              TextButton(
+                                child: Text("Adicionar"),
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                        "Deck adicionado com sucesso!",
+                                      ),
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  );
+                                  Navigator.pushNamed(context, "/user");
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     } else {
-                      idDeck = null;
+                      // showDialog(
+                      //   context: context,
+                      //   barrierDismissible: false,
+                      //   builder: (BuildContext context) {
+                      //     return AlertDialog(
+                      //       title: Text("Erro"),
+                      //       content: Text(
+                      //         "Não foi possivel encontrar o deck, gostaria de Digitar manualmente?",
+                      //       ),
+                      //       actions: [
+                      //         TextButton(
+                      //           child: Text("Sim"),
+                      //           onPressed: () {
+                      //             Navigator.of(context).pop();
+                      //             showDialog(
+                      //               context: context,
+                      //               barrierDismissible: false,
+                      //               builder: (BuildContext context) {
+                      //                 return AlertDialog(
+                      //                   title: Text("Digite a ID do deck!"),
+                      //                   content: TextField(
+                      //                     decoration: InputDecoration(
+                      //                       hintText: "Id do deck",
+                      //                     ),
+                      //                     onSubmitted: (idDeck) {
+                      //                       if (idDeck.length == 17) {
+                      //                         Navigator.of(context).pop();
+                      //                         ScaffoldMessenger.of(
+                      //                           context,
+                      //                         ).showSnackBar(
+                      //                           SnackBar(
+                      //                             content: Text(
+                      //                               "Deck adicionado com sucesso!",
+                      //                             ),
+                      //                             duration: Duration(
+                      //                               seconds: 2,
+                      //                             ),
+                      //                           ),
+                      //                         );
+                      //                       } else {
+                      //                         ScaffoldMessenger.of(
+                      //                           context,
+                      //                         ).showSnackBar(
+                      //                           SnackBar(
+                      //                             content: Text(
+                      //                               "Não foi possível adicionar o deck!",
+                      //                             ),
+                      //                             duration: Duration(
+                      //                               seconds: 2,
+                      //                             ),
+                      //                           ),
+                      //                         );
+                      //                       }
+                      //                     },
+                      //                   ),
+                      //                 );
+                      //               },
+                      //             ); //Show Dialog
+                      //           },
+                      //         ),
+                      //       ],
+                      //     );
+                      //   },
+                      // );
                     }
                   });
-                } //Final do for
-                if (idDeck == null) {
-                } else {
-                  // cameraController.stop();
-                  Navigator.pushNamed(
-                    context,
-                    '/scannerResult',
-                    arguments: idDeck,
-                  );
                 }
               },
             ),
-            
           ),
-        
+          Positioned(
+            top: 40,
+            right: 20,
+            child: FilledButton.icon(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Digite a ID do deck!"),
+                      content: TextField(
+                        decoration: InputDecoration(hintText: "Id"),
+                      ),
+                      actions: [
+                        TextButton(
+                          child: Text("Cancelar"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("Deck Nâo foi adicionado!"),
+                                duration: Duration(seconds: 2),
+                              ),
+                            );
+                          },
+                        ),
+
+                        TextButton(
+                          child: Text("Adicionar"),
+                          onPressed: () {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("Deck adicionado com sucesso!"),
+                                duration: Duration(seconds: 2),
+                              ),
+                            );
+                            Navigator.pushNamed(context, "/user");
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              icon: Icon(Icons.edit),
+              label: Text("Inserir ID"),
+            ),
+          ),
         ],
       ),
 
--- lib/pages/scannerresult.dart
+++ lib/pages/scannerresult.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:forgebase/pages/idscanner.dart';
+// import 'package:flutter/material.dart';
+// import 'package:forgebase/pages/idscanner.dart';
 
-class ScannerResult extends StatefulWidget {
-  const ScannerResult({super.key});
+// class ScannerResult extends StatefulWidget {
+//   const ScannerResult({super.key});
 
-  @override
-  State<ScannerResult> createState() => _ScannerResultState();
-}
+//   @override
+//   State<ScannerResult> createState() => _ScannerResultState();
+// }
 
-class _ScannerResultState extends State<ScannerResult> {
-  @override
-  Widget build(BuildContext context) {
-    final String? idDeck = ModalRoute.of(context)?.settings.arguments as String?;
-    ModalRoute.of(context)?.settings.arguments as String?;
-    return Scaffold(
-      body: Text("d"), 
-      appBar: AppBar(),
-      );
-  }
-}
+// class _ScannerResultState extends State<ScannerResult> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final String? idDeck = ModalRoute.of(context)?.settings.arguments as String?;
+//     ModalRoute.of(context)?.settings.arguments as String?;
+//     return Scaffold(
+//       body: Text("d"), 
+//       appBar: AppBar(),
+//       );
+//   }
+// }
