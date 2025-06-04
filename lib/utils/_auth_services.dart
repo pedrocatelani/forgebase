3aa1d908a195bc69d56687422cd4b2e92134d295
Tales Brandt <tales.bnascimento@gmail.com>
2025-06-04 15:48:26 -0300

    refactor(*): Multiple fixes in firebase auth

--- lib/utils/_auth_services.dart
+++ lib/utils/_auth_services.dart
@@ -1,17 +1,35 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:forgebase/utils/_firebase_collections.dart';
+import 'package:forgebase/utils/dok_api.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseColletion _database = FirebaseColletion();
+  final dokApi = DoKApi();
 
-  Future<User?> login(String email, String password) async {
-    UserCredential credential = await _auth.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    return credential.user;
+  Future<void> login(
+    String email,
+    String password,
+    BuildContext context,
+  ) async {
+    try {
+      UserCredential credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // ignore: unnecessary_null_comparison
+      if (credential != null) {
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacementNamed(context, '/home');
+      }
+      // ignore: unused_catch_clause
+    } on FirebaseAuthException catch (ex) {
+      final snackBar = SnackBar(content: Text("Email or password invalid"));
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 
   Future<void> register(
@@ -20,27 +38,38 @@ class AuthService {
     String password,
     String confirmPassword,
     // ignore: non_constant_identifier_names
-    String APIKey,
+    String apiKey,
     BuildContext context,
   ) async {
     try {
       if (password == confirmPassword) {
-        UserCredential credential = await _auth.createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-        await credential.user!.updateDisplayName(name);
+        String dokiD = 'c340a4e0-eec2-4b80-b333-178b65b6f596';
+        final result = await dokApi.getStatistics(dokiD, apiKey);
 
-        var data = {'user': email, 'api_key': APIKey, 'user_image': ''};
+        if (result['status'] == 200) {
+          UserCredential credential = await _auth
+              .createUserWithEmailAndPassword(email: email, password: password);
+          await credential.user!.updateDisplayName(name);
 
-        await _database.insertUser(email, data);
+          var data = {'user': email, 'api_key': apiKey, 'user_image': ''};
 
+          await _database.insertUser(email, data);
+
+          // ignore: use_build_context_synchronously
+          Navigator.pushReplacementNamed(context, "/home");
+        } else {
+          final snackBar = SnackBar(content: Text("APIKey invaled"));
+          // ignore: use_build_context_synchronously
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        }
+      } else {
+        final snackBar = SnackBar(content: Text("The passwords are different"));
         // ignore: use_build_context_synchronously
-        Navigator.pushReplacementNamed(context, "/home");
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
       }
       // ignore: unused_catch_clause
     } on FirebaseAuthException catch (ex) {
-      final snackBar = SnackBar(content: Text("Error"));
+      final snackBar = SnackBar(content: Text("Error in register"));
       // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
     }
@@ -85,33 +114,44 @@ class AuthService {
     String apiKey,
     BuildContext context,
   ) async {
-    try {
+    if (userName != _auth.currentUser?.displayName) {
       _auth.currentUser?.updateDisplayName(userName);
       final snackBar = SnackBar(content: Text("Updated name"));
       // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    } catch (ex) {
-      final snackBar = SnackBar(content: Text("Error"));
-      // ignore: use_build_context_synchronously
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    }
-
-    final isApiKeyValid = await _database.updateApiKey(
-      userEmail,
-      apiKey,
-      context,
-    );
-
-    if (isApiKeyValid) {
-      // ignore: use_build_context_synchronously
-      Navigator.pushReplacementNamed(context, '/user');
-      final snackBar = SnackBar(content: Text("Updated Api Key"));
-      // ignore: use_build_context_synchronously
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
     } else {
-      final snackBar = SnackBar(content: Text("Api Key invalid"));
+      final snackBar = SnackBar(
+        content: Text("Current name same as previous one"),
+      );
       // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
     }
+
+    var oldApiKey = await _database.getApiKey(userEmail);
+
+    if (apiKey != oldApiKey) {
+      final isApiKeyValid = await _database.updateApiKey(
+        userEmail,
+        apiKey.trim(),
+      );
+
+      if (isApiKeyValid) {
+        final snackBar = SnackBar(content: Text("Updated Api Key"));
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      } else {
+        final snackBar = SnackBar(content: Text("Api Key invalid"));
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    } else {
+      final snackBar = SnackBar(
+        content: Text("Current APIKey same as previous one"),
+      );
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacementNamed(context, '/user');
   }
 }
